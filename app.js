5a93fb411f3a2800a4e9d738fbc45536ef70e24e
Michael Ebube <michaelebube18@gmail.com>
2023-03-10 01:14:48 +0100

    New Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/* DOM */
+
+//document.body.style.backgroundColor = 'blue';
+//document.body.style.color = 'yellow'
+//const element = document.
+
+/* Window Object */
+console.log(window)
+ console.dir(document)
+
+ /* getElementById('element') */
+const h1 = document.getElementById("title");
+h1.style.color = "red";
